30cac4148f1323f52b4f85ab87a182175a18ab77
Urszula Majcher <ulunka.m@gmail.com>
2024-10-08 21:46:04 +0200

    added currence table

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    name: "EUR",
+    name: "Euro",
+    rate: 4.3203,
+  },
+  {
+    name: "GBP",
+    name: "Funt brytyjski",
+    rate: 5.1478,
+  },
+  {
+    name: "USD",
+    name: "Dolar amerykański",
+    rate: 3.9299,
+  },
+];
