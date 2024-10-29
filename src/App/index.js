4cc6a3dc1ad026de129a3085620dfd6c61d16110
Urszula Majcher <ulunka.m@gmail.com>
2024-10-29 18:54:14 +0100

    corrected function App

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
 import "./App.css";
 import { Form } from "./Form";
 
 function App() {
   return (
     <div className="app">
-      <Form result={result} calculateResult={calculateResult} />
+      <Form />
     </div>
   );
 }
